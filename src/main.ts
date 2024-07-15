57f9a83cae9b138bd164614fa51cd5cafea0bfea
Szymon Wrzos <szymon.wrzos@bitapps.fi>
2024-07-15 16:13:38 +0200

    Added layers and view

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { default as OLMap } from "ol/Map";
 import "./style.css";
+import { View } from "ol";
+import { contours, countries } from "./layers";
 
 const element = document.getElementById("app")!!;
 
-const map = new OLMap({
+const view = new View({
+  center: [0, 0],
+  zoom: 12,
+});
+
+new OLMap({
   target: element,
+  view,
+  controls: [],
+  layers: [countries, contours],
 });
